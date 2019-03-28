531806ca26fa76747cce65b15ad0a502a589b0e3
Robsonsjre <robsonsjre@gmail.com>
2019-03-28 22:44:26 +0100

    added Roboto font

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import { createRootReducer } from "./reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import './index.css';
+import 'typeface-roboto'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
